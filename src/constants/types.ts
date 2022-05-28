5aa2256bad3e2f550c791c44c13716b427e3ebd2
Unekwe Francis <unekwenchekwube@gmail.com>
2022-05-28 18:27:20 +0100

    added assets

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,6 +1,16 @@
+export const home = 'Home';
+export const search = 'Search';
+export const library = 'Library';
+
 export interface CounterState {
   value: number;
 }
 export interface OnboardingState {
   firstTime: boolean;
 }
+
+export type MainBottomTabParams = {
+  Home: undefined;
+  Search: undefined;
+  Library: undefined;
+};
